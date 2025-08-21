3e8661a710a5d31d617329f3711cc294b87f70dd
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-21 23:50:57 +0530

    Create Footer.tsx

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-100 text-center p-4 text-sm">
+      © 2025 Beardev Bank. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
