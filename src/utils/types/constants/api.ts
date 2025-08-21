9c917b40d683af14f2508da54dcd795fae28573d
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 00:19:17 +0530

    constants directory

--- src/utils/types/constants/api.ts
+++
@@ -1,7 +0,0 @@
-export const API_ENDPOINTS = {
-  login: '/api/auth/login',
-  signup: '/api/auth/signup',
-  fetchDashboard: '/api/dashboard',
-  fetchInvestments: '/api/investments',
-  fetchSavings: '/api/savings',
-};
