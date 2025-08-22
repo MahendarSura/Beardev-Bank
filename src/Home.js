6c9f55feebbf72df5b4e176c052bc1d234d2f1c4
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 22:17:27 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold text-axisRed">Welcome to Beardev Bank</h1>
+      <p className="mt-4 text-gray-700">Your digital banking dashboard</p>
+    </div>
+  );
+}
+
+export default Home;
