3e31acbae91dbd2a9b6544722c2f9ca628ca3051
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 18:54:04 +0530

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold text-axisRed">Welcome to Beardev Bank</h1>
+      <p className="mt-4 text-lg">This is your home page content.</p>
+    </div>
+  );
+};
+
+export default Home;
