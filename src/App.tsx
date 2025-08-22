dd4c3a26a64cc39e303c961e4620f82ecec2eede
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 20:41:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/index";
@@ -13,24 +12,22 @@ function App() {
       {/* NAVBAR */}
       <Navbar />
 
-      {/* MAIN CONTENT */}
-      <main className="flex-1 p-4 space-y-6">
-        {/* Example Axis Red Text */}
-        <div className="text-axisRed hover-text-axisRed text-2xl font-bold">
-          Axis Red Text
-        </div>
-
-        {/* Example Axis Red Background */}
-        <div className="bg-axisRed hover-bg-axisRed p-4 rounded text-white font-semibold">
-          Axis Red Background
-        </div>
-
-        {/* ROUTES */}
+      <main className="flex-1 p-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
+
+        {/* Tailwind test blocks */}
+        <div className="mt-8 space-y-4">
+          <div className="text-axisRed hover-text-axisRed text-2xl font-bold">
+            Axis Red Text
+          </div>
+          <div className="bg-axisRed hover-bg-axisRed p-4 rounded text-white font-semibold">
+            Axis Red Background
+          </div>
+        </div>
       </main>
 
       {/* FOOTER */}
