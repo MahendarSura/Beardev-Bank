6c744005b7e2d6af40422827bb34cf2fea0d68a7
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 20:30:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/index';
-import Login from './pages/login';
-import Dashboard from './pages/dashboard';
+// src/App.tsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/index";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="p-4 bg-axisRed text-white">Navbar</header>
+      <Navbar />
       <main className="flex-1 p-4">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -14,7 +18,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </main>
-      <footer className="p-4 bg-gray-800 text-white">Footer</footer>
+      <Footer />
     </div>
   );
 }
