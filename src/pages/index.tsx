17fa8e52b1222478a8f2dde405ddee0ffc304b80
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 19:01:55 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+// src/pages/index.tsx
 const Home = () => {
-  return <h1 className="text-2xl font-bold">Welcome to Beardev Bank 🏦</h1>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-axisRed">
+        Welcome to Beardev Bank 🏦
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
