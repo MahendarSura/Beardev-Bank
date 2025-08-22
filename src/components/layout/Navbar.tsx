633dcfc30bec281c0d0116eaf67cbbaece7ffac5
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 22:16:06 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between">
-      <Link to="/" className="font-bold">Beardev Bank</Link>
-      <div className="space-x-4">
-        <Link to="/login">Login</Link>
-        <Link to="/dashboard">Dashboard</Link>
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <div className="text-2xl font-bold">Beardev Bank</div>
+      <div>
+        <a href="/" className="mx-2 hover:text-gray-200">Home</a>
+        <a href="/login" className="mx-2 hover:text-gray-200">Login</a>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
