518eb455d0c55b389f9fe2bf86bfe168ad60acbe
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 19:02:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
+// src/App.tsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/index';
-import Login from './pages/login';
-import Dashboard from './pages/dashboard';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
+// import Navbar from './components/layout/Navbar';
+// import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="flex-1 p-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
