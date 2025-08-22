fbc4c4340ef7d92e7654b87a3e013c4c30a57c91
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 20:35:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,30 @@ import Footer from "./components/layout/Footer";
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      {/* NAVBAR */}
       <Navbar />
-      <main className="flex-1 p-4">
+
+      {/* MAIN CONTENT */}
+      <main className="flex-1 p-4 space-y-6">
+        {/* Example Axis Red Text */}
+        <div className="text-axisRed hover-text-axisRed text-2xl font-bold">
+          Axis Red Text
+        </div>
+
+        {/* Example Axis Red Background */}
+        <div className="bg-axisRed hover-bg-axisRed p-4 rounded text-white font-semibold">
+          Axis Red Background
+        </div>
+
+        {/* ROUTES */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </main>
+
+      {/* FOOTER */}
       <Footer />
     </div>
   );
