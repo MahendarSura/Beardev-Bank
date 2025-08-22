c33657a36ac02c6920e386299c10d46c223ee7af
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-22 20:20:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      input: '/index.html',
+  resolve: {
+    alias: {
+      '/src/main.jsx': path.resolve(__dirname, 'src/main.tsx'),
     },
   },
 });
