085b7eaab4be1d0661591d467e3ea910d4fbbb3c
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-23 22:32:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,35 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Sidebar from "./components/layout/Sidebar";
-import Home from "./Home"; 
+import Home from "./Home";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <div className="flex h-screen">
+      {/* Sidebar */}
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+
+      {/* Main content area */}
+      <div className="flex flex-col flex-1">
+        {/* Navbar */}
         <Navbar />
-        <div className="flex h-screen">
-          <Footer />
-          <div className="flex h-screen">
-            <Login />
-            <div className="flex h-screen">
-              <Home />
-              <main className="p-4 flex-1 overflow-auto">
-               <Dashboard />
-             </main>
-           </div>
-         </div>
-       );
-     }
+
+        {/* Page content */}
+        <main className="flex-1 overflow-auto p-4 bg-gray-100">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </div>
+  );
+}
 
 export default App;
