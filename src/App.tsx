eda11023f490ec80eda004b4ad9e93bbe2c21096
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-23 16:17:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
