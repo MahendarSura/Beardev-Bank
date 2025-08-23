840149ece4567a5112b6e5165c36c41d8724edd1
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-23 16:41:26 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-// src/main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App"; 
-import "./index.css"; 
+import React from 'react';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+function Home() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold text-axisRed">Welcome to Beardev Bank</h1>
+      <p className="mt-4 text-gray-700">Your digital banking dashboard</p>
+    </div>
+  );
+}
+
+export default Home;
+
