afa6a9040ed89755936a08fa698747d5356dfddf
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-25 17:45:23 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-axisRed">
-        Welcome to Beardev Bank 🏦
+        Beardev Bank 
       </h1>
     </div>
   );
