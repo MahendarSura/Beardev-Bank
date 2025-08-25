2a1fc2b8c394be8fed4c512298441ad3e65a1c05
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-25 17:20:16 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,6 @@ const Home = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-axisRed">
         Beardev Bank 
-        <nav className="flex items-center justify-between mb-12">
-        <div className="text-5xl font-extrabold text-axisRed">Beardev Bank</div>
-        <ul className="hidden md:flex space-x-10 font-semibold text-gray-700">
-          <li className="hover:text-axisRed cursor-pointer">Explore products</li>
-          <li className="hover:text-axisRed cursor-pointer">Grab Deal</li>
-          <li className="hover:text-axisRed cursor-pointer">Make Payments</li>
-          <li className="hover:text-axisRed cursor-pointer">Bank Smart</li>
-          <li className="hover:text-axisRed cursor-pointer">Accounts</li>
-          <li className="hover:text-axisRed cursor-pointer">Deposits</li>
-          <li className="hover:text-axisRed cursor-pointer">Cards</li>
-          <li className="hover:text-axisRed cursor-pointer">Loans</li>
-          <li className="hover:text-axisRed cursor-pointer">Investments</li>
-          <li className="hover:text-axisRed cursor-pointer">Insurance</li>
-          <li className="hover:text-axisRed cursor-pointer">Special Services</li>
-          <li className="hover:text-axisRed cursor-pointer">Knowledge Hub</li>
-        </ul>
-        <div className="text-axisRed font-semibold cursor-pointer hover:underline">Login/Signup</div>
-      </nav>
       </h1>
     </div>
   );
