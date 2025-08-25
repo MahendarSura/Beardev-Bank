c5deebaafeb7571cf1b9156aea7f4e31eb046b9d
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-25 16:44:29 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
-      <div className="text-2xl font-bold">Beardev Bank</div>
-      <div>
-        <a href="/" className="mx-2 hover:text-gray-200">Home</a>
-        <a href="/login" className="mx-2 hover:text-gray-200">Login</a>
+    <nav className="bg-blue-600 text-white p-4">
+      {/* Center wrapper */}
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <div className="text-2xl font-bold">Beardev Bank</div>
+        <div>
+          <a href="/" className="mx-2 hover:text-gray-200">Home</a>
+          <a href="/login" className="mx-2 hover:text-gray-200">Login</a>
+        </div>
       </div>
     </nav>
   );
