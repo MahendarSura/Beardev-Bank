b38b2892fd0f277d9d54adb4bc8a0c778b8ef1a6
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-25 22:31:08 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        lg: "2rem",
+      },
+    },
+    extend: {},
+  },
   plugins: [],
 };
