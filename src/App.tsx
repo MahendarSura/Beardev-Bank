c43125f35de77924a706994282cce6e7a2cc5c9a
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-25 21:45:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Home from "./pages/index"; // Assuming your Home component is in src/pages/index.tsx
-import Login from "./pages/login";
-import Dashboard from "./pages/dashboard";
+import Home from "./pages/Home";  // <-- make sure this path is correct
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
-      <Navbar />
-      <main className="flex-1 p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* other routes */}
-        </Routes>
-      </main>
-      <Footer />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
