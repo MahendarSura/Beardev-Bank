c94d0c286fb43cc92d1b0fb69e49e9b38e980cb3
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-25 17:47:33 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className="bg-white shadow rounded-md flex items-center justify-between py-4 px-6 border border-gray-200">
-      <div className="text-3xl font-extrabold text-[#E80000] cursor-pointer">ICICI Bank</div>
       <ul className="hidden md:flex space-x-8 font-semibold text-gray-700">
         <li className="hover:text-[#E80000] cursor-pointer">Personal</li>
         <li className="hover:text-[#E80000] cursor-pointer">NRI</li>
