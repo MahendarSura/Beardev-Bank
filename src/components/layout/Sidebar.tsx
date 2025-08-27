8105f2851a08de2100df73611cc23fc66c7585c0
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-27 14:38:02 +0530

    Update Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,15 +1,1 @@
-import React from "react";
 
-function Sidebar() {
-  return (
-    <aside className="w-64 bg-gray-800 text-white p-4">
-      <ul>
-        <li className="mb-2 hover:bg-gray-700 p-2 rounded cursor-pointer">Dashboard</li>
-        <li className="mb-2 hover:bg-gray-700 p-2 rounded cursor-pointer">Home</li>
-        <li className="mb-2 hover:bg-gray-700 p-2 rounded cursor-pointer">Peer Review</li>
-      </ul>
-    </aside>
-  );
-}
-
-export default Sidebar;
