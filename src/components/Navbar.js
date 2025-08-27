b5730ce77624fcfe69d75e1b480477b84c8e1e7e
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-27 14:45:40 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,14 @@
 
+
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl font-bold">Beardev Bank</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
+
