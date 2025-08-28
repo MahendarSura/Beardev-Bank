6aaab7ecf996a877b9d98a9163831eeff075ed20
Mahendar Sura <mahendar.sura8@gmail.com>
2025-08-28 20:06:48 +0530

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -182,7 +182,6 @@ const App = () => {
       "Ways to Bank", "Insta Services", "Business & Fintech", "Open Banking"
     ]
   };
-
   return (
     <div className="bg-gray-100 font-sans min-h-screen">
       <div className="px-4 sm:px-6 lg:px-16 xl:px-20">
